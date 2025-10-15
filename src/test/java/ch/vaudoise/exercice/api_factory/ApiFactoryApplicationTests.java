4c51d486f5a9e448266734c49808c14b46ff59d7
daniel.mihalcio <daniel.mihalcioiu@edu.hefr.ch>
2025-10-15 16:47:42 +0200

    Initial Spring Boot, Maven & Postgre setup

---
+++ src/test/java/ch/vaudoise/exercice/api_factory/ApiFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.vaudoise.exercice.api_factory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
